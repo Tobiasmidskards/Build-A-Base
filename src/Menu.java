4e3b6fc181e3cffd63c257182f1a385483c2f99e
andreasdan <andreas.petersen01@gmail.com>
2018-11-08 11:57:35 +0100

    menu functionality

--- src/Menu.java
+++ src/Menu.java
@@ -4,41 +4,66 @@ public class Menu {
 
 	private DataController dataController;
 	private Scanner scanner;
+	private MenuState state;
 
 	public Menu() {
-      this.dataController = new DataController();
+    this.dataController = new DataController();
 		this.scanner = new Scanner(System.in);
+		state = MenuState.MAINMENU;
 	}
 
-	public boolean displayMenu() {
+	public void displayMenu() {
+		switch(state) {
+			case MAINMENU:
+				displayMainMenu();
+				break;
+			case LOGIN:
+				displayLoginMenu();
+				break;
+			case SEARCH:
+				displayLoginMenu();
+				break;
+		}
+	}
+
+	public void displayMainMenu() {
 		System.out.println("*** Build A Base ***");
-		System.out.println("0 : Exit")
+		System.out.println("0 : Exit");
 		System.out.println("1 : Login");
 		System.out.println("2 : Database lookup");
 
-		while (scanner.hasNextInt())
-		{
-			int input = scanner.nextInt();
-			if (input == 0)
-			{
-				return false;
-			}
-			else if (input == 1)
-			{
-				displayLoginMenu();
-			}
-			else if (intput == 2)
-			{
-				displayDatabaseLookupMenu();
-			}
-		}
+		// while (scanner.hasNextInt())
+		// {
+		// 	int input = scanner.nextInt();
+		// 	if (input == 0)
+		// 	{
+		// 		return false;
+		// 	}
+		// 	else if (input == 1)
+		// 	{
+		// 		displayLoginMenu();
+		// 	}
+		// 	else if (intput == 2)
+		// 	{
+		// 		displayDatabaseLookupMenu();
+		// 	}
+		// }
 
-		return true;
+		//return true;
 	}
 
 	public void displayLoginMenu() {
-		// TODO - implement Menu.displayStaffMenu
-		throw new UnsupportedOperationException();
+		System.out.println("*** Build A Base ***");
+		System.out.print("Username: ");
+	 	String username = scanner.nextLine();
+		System.out.print("Password: ");
+		String password = scanner.nextLine();
+		// if (login(username, password)) {
+		// 	state.SEARCH;
+		// } else {
+		// 	state.LOGIN;
+		// }
+
 	}
 
 	public void displayDatabaseLookupMenu() {
---
+++ src/MenuState.java
@@ -0,0 +1,5 @@
+public enum MenuState{
+  LOGIN,
+  SEARCH,
+  MAINMENU
+}
