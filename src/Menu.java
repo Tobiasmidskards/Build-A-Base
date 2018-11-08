c1e1197c286e28e76c00156cb53a03763f96add6
andreasdan <andreas.petersen01@gmail.com>
2018-11-08 11:07:42 +0100

    menu

--- src/Menu.java
+++ src/Menu.java
@@ -11,15 +11,37 @@ public class Menu {
 	}
 
 	public boolean displayMenu() {
+		System.out.println("*** Build A Base ***");
+		System.out.println("0 : Exit")
+		System.out.println("1 : Login");
+		System.out.println("2 : Database lookup");
+
+		while (scanner.hasNextInt())
+		{
+			int input = scanner.nextInt();
+			if (input == 0)
+			{
+				return false;
+			}
+			else if (input == 1)
+			{
+				displayLoginMenu();
+			}
+			else if (intput == 2)
+			{
+				displayDatabaseLookupMenu();
+			}
+		}
+
 		return true;
 	}
 
-	public void displayStaffMenu() {
+	public void displayLoginMenu() {
 		// TODO - implement Menu.displayStaffMenu
 		throw new UnsupportedOperationException();
 	}
 
-	public void displayLoginPrompt() {
+	public void displayDatabaseLookupMenu() {
 		// TODO - implement Menu.displayLoginPrompt
 		throw new UnsupportedOperationException();
 	}
